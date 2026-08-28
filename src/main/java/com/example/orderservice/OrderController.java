e0b442a923b5c051ccf87404b7706e7160861126
subhalakshmisiva <subhalakshmisiva@gmail.com>
2026-08-28 21:47:59 +0530

    Fix inter-service URL hardcoding using @Value annotations

--- src/main/java/com/example/orderservice/OrderController.java
+++ src/main/java/com/example/orderservice/OrderController.java
@@ -3,14 +3,18 @@ package com.example.orderservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.beans.factory.annotation.Value;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 
     @Autowired
     private RestTemplate restTemplate;
+   @Value("${services.user-service.url}")
+    private String userServiceBaseUrl;
 
+    @Value("${services.product-service.url}")
+    private String productServiceBaseUrl;
     @GetMapping("/{orderId}/user/{userId}/product/{productId}")
     public OrderResponseDTO getOrderDetails(
             @PathVariable Long orderId,
@@ -18,11 +22,11 @@ public class OrderController {
             @PathVariable Long productId) {
 
         // Fetch user from User Service (port 8081)
-        String userServiceUrl = "http://localhost:8081/users/" + userId;
+        String userServiceUrl = userServiceBaseUrl + "/users/" + userId;
         UserDTO user = restTemplate.getForObject(userServiceUrl, UserDTO.class);
 
         // Fetch product from Product Service (port 8082)
-        String productServiceUrl = "http://localhost:8082/products/" + productId;
+        String productServiceUrl = productServiceBaseUrl + "/products/" + productId;
         ProductDTO product = restTemplate.getForObject(productServiceUrl, ProductDTO.class);
 
         // Aggregate response
